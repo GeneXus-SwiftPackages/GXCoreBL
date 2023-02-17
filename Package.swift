696f720b541dae4f079f1d46aa16890ee5739141
Build Er <builder@macmini6.local>
2023-02-16 23:49:17 -0300

    Package version: 1.0.0-beta+20230217024255

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXCoreBL",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXCoreBL",
+			targets: ["GXCoreBLWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXStandardClasses.git", branch: "beta")
+	],
+	targets: [
+		.target(name: "GXCoreBLWrapper",
+				dependencies: [
+					"GXCoreBL",
+					.product(name: "GXStandardClasses", package: "GXStandardClasses", condition: .when(platforms: [.tvOS, .watchOS, .iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXCoreBL",
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreBL-1.0.0-beta+20230217024255.xcframework.zip",
+			checksum: "48ab23875ee29c4039381d1dc88628ff647b769172c0026bf7e084fe4cd2ba80"
+		)
+	]
+)
