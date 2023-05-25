49172e270a3a91a90c2b5b4caa7f50bfdba58427
Build Er <builder@macmini6.local>
2023-05-25 09:35:56 -0300

    Package version: 1.0.0-beta+20230525122919

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreBL",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreBL-1.0.0-beta+20230524184838.xcframework.zip",
-			checksum: "d2275186413397ab6fda9dca9128df0b544939747f14b1d45627c9fc5b2106bc"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreBL-1.0.0-beta+20230525122919.xcframework.zip",
+			checksum: "9a479dbcc47d2ace84f5985c460f9b26f5ba0b5c10122533b539cc4981717e2d"
 		)
 	]
 )
