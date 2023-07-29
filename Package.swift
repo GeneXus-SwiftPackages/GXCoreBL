26303b4d93abf7fc6cc676d2afaa7586ba628bff
genexusbot <builder@genexus.com>
2023-07-29 18:41:43 -0300

    Package version: 1.1.0-rc.1

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXCoreBL",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXCoreBL",
+			targets: ["GXCoreBLWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXStandardClasses.git", exact: "1.1.0-rc.1")
+	],
+	targets: [
+		.target(name: "GXCoreBLWrapper",
+				dependencies: [
+					"GXCoreBL",
+					.product(name: "GXStandardClasses", package: "GXStandardClasses", condition: .when(platforms: [.tvOS, .iOS, .watchOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXCoreBL",
+			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreBL-1.1.0-rc.1.xcframework.zip",
+			checksum: "acf128a45c53fe63b438115d8c8c82701d1e6b4a693dadc117178017d8744862"
+		)
+	]
+)
