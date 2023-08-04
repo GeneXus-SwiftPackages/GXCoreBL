e7557656ab65d157f64a9296234d98ff8d7c510a
Build Er <builder@macmini6.local>
2023-08-04 16:30:40 -0300

    Package version: 1.1.0-rc.5

--- Package.swift
+++ Package.swift
@@ -10,19 +10,19 @@ let package = Package(
 			targets: ["GXCoreBLWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXStandardClasses.git", exact: "1.1.0-rc.4")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXStandardClasses.git", exact: "1.1.0-rc.5")
 	],
 	targets: [
 		.target(name: "GXCoreBLWrapper",
 				dependencies: [
 					"GXCoreBL",
-					.product(name: "GXStandardClasses", package: "GXStandardClasses", condition: .when(platforms: [.tvOS, .iOS, .watchOS]))
+					.product(name: "GXStandardClasses", package: "GXStandardClasses", condition: .when(platforms: [.watchOS, .tvOS, .iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreBL",
-			url: "https://pkgs.genexus.dev/iOS/preview/GXCoreBL-1.1.0-rc.4.xcframework.zip",
-			checksum: "fe55ffd47cd9cb7dccc5468151fbbf59c1d2bb9ff1f203f0adcd7417b88922ef"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXCoreBL-1.1.0-rc.5.xcframework.zip",
+			checksum: "77df95c96c3e2ccd13c72f4caa39c450bf7e0cd5108aa3c61e32647568165075"
 		)
 	]
 )
