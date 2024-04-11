54368520879bdceb9a629c06846887b8b3ec7b67
genexusbot <builder@genexus.com>
2024-04-11 17:41:07 -0300

    Package version: 1.6.0-rc.0

--- Package.swift
+++ Package.swift
@@ -3,26 +3,26 @@ import PackageDescription
 
 let package = Package(
 	name: "GXCoreBL",
-	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	platforms: [.iOS("12.0"), .watchOS("9.0"), .tvOS("12.0")],
 	products: [
 		.library(
 			name: "GXCoreBL",
 			targets: ["GXCoreBLWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXStandardClasses.git", exact: "1.1.0")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXStandardClasses.git", exact: "1.6.0-rc.0")
 	],
 	targets: [
 		.target(name: "GXCoreBLWrapper",
 				dependencies: [
 					"GXCoreBL",
-					.product(name: "GXStandardClasses", package: "GXStandardClasses", condition: .when(platforms: [.watchOS, .tvOS, .iOS]))
+					.product(name: "GXStandardClasses", package: "GXStandardClasses", condition: .when(platforms: [.iOS, .watchOS, .tvOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreBL",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreBL-1.1.0.xcframework.zip",
-			checksum: "ff22244534a78585b806edcb517ea57a67f3b3622da80996833f4ce72d2cd34e"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXCoreBL-1.6.0-rc.0.xcframework.zip",
+			checksum: "c17343c5e04a20f3fb12f2846051b89db057947c60c2e48bb7fa471e47a7b2ba"
 		)
 	]
 )
