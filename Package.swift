104cc7bfaa5dcc003b34c9d484f184286bed886b
The iOS Team @ GeneXus <builder@genexus.com>
2025-01-02 22:39:42 -0300

    Package version: 2.2.0-beta.30

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreBLWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXStandardClasses.git", exact: "2.2.0-beta.26")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXStandardClasses.git", exact: "2.2.0-beta.30")
 	],
 	targets: [
 		.target(name: "GXCoreBLWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreBL",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreBL-2.2.0-beta.26.xcframework.zip",
-			checksum: "8263d4067e00fd7bbc4840cabcf4ca42225c942daf37ab6bc1a72eb49c0bb45c"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreBL-2.2.0-beta.30.xcframework.zip",
+			checksum: "66ba364ee01272939091fe3fe0c60b78fa645a8c1428ce1d225c0adf11f9cb5f"
 		)
 	]
 )
